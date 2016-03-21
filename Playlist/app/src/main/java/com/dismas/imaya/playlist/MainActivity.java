b5b0c249f0aeb5a1747c34c774f8693c19a2875a
ImayaDismas <imayadismas@gmail.com>
2016-03-21 08:56:27 +0300

    Added the youtube library

--- Playlist/app/src/main/java/com/dismas/imaya/playlist/MainActivity.java
+++ Playlist/app/src/main/java/com/dismas/imaya/playlist/MainActivity.java
@@ -1,7 +1,12 @@
 package com.dismas.imaya.playlist;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.google.android.youtube.player.YouTubeApiServiceUtil;
+import com.google.android.youtube.player.YouTubeInitializationResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Only add the toolbar if we are on Honeycomb and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+        }
+
+
+        //Check for any issues
+        final YouTubeInitializationResult result = YouTubeApiServiceUtil.isYouTubeApiServiceAvailable(this);
+
+        if (result != YouTubeInitializationResult.SUCCESS) {
+            //If there are any issues we can show an error dialog.
+            result.getErrorDialog(this, 0).show();
+        }
     }
 }
